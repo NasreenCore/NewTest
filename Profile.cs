2cd708c239f4fb296294bd9b00bf18f4db7ab9a1
NasreenCore <nbanonet@gmail.com>
2026-05-26 15:46:36 +0530

    profileTaskCompleted

---
+++ Profile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTest
+{
+    public class Profile
+    {
+        public void printName(string name)
+        { Console.WriteLine(name); }
+        public void printDescription(string description) { Console.WriteLine(description); }
+    }
+}
