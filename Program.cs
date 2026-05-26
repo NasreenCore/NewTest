7cc61f443e3d177e4d01c4e33ff7e3970a5b145f
NasreenCore <nbanonet@gmail.com>
2026-05-26 14:10:11 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NewTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am going to test the git repository...");
+        }
+    }
+}
